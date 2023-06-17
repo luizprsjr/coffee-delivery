304f959d0851b13d140f420c5e6b58edb605031b
Luiz Pires <lpiresjunior@gmail.com>
2023-06-17 03:57:12 -0300

    feat: animate splash screen

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,20 +1,104 @@
 import { useRouter } from 'expo-router'
 import { useEffect } from 'react'
-import { Button, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, View } from 'react-native'
+import Animated, {
+  Keyframe,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
+
+import BrandName from '../src/assets/brand-name.svg'
+import Logo from '../src/assets/logo.svg'
+
+const SIZE = Dimensions.get('screen').height + 200
 
 export default function Splash() {
   const router = useRouter()
 
-  useEffect(() => {
+  function goBack() {
     setTimeout(() => {
       router.push('/')
-    }, 3000)
-  }, [router])
+    }, 1000)
+  }
+
+  const scale = useSharedValue(0)
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    }
+  })
+
+  const enteringKeyFrame = new Keyframe({
+    from: {
+      opacity: 0.9,
+      transform: [{ translateX: 0 }],
+    },
+    to: {
+      opacity: 1,
+      transform: [{ translateX: -55 }],
+    },
+  })
+
+  const brandEnteringKeyFrame = new Keyframe({
+    from: {
+      opacity: 0,
+      transform: [{ translateX: 100 }],
+    },
+    to: {
+      opacity: 1,
+      transform: [{ translateX: 29 }],
+    },
+  })
+
+  useEffect(() => {
+    scale.value = withTiming(1, { duration: 1000 })
+  }, [scale])
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Splash</Text>
-      <Button title="home" onPress={() => router.push('/')} />
+    <View style={styles.container}>
+      <Animated.View style={[styles.animatedBackground, reanimatedStyle]} />
+
+      <Animated.View
+        entering={enteringKeyFrame.delay(1000).duration(500)}
+        style={{ position: 'absolute' }}
+      >
+        <Logo />
+      </Animated.View>
+
+      <Animated.View
+        entering={brandEnteringKeyFrame
+          .delay(1000)
+          .duration(500)
+          .withCallback((finished) => {
+            'worklet'
+            if (finished) {
+              runOnJS(goBack)()
+            }
+          })}
+        style={{ position: 'absolute' }}
+      >
+        <BrandName />
+      </Animated.View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4B2995',
+  },
+  animatedBackground: {
+    height: SIZE,
+    width: SIZE,
+    borderRadius: SIZE,
+    backgroundColor: '#8047F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
