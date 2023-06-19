ad76fed13ef2b9226fc31fbc80fe863f2a192cae
Luiz Pires <lpiresjunior@gmail.com>
2023-06-19 04:08:14 -0300

    feat: create app theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+
+    gray_100: '#272221',
+    gray_200: '#403937',
+    gray_300: '#574F4D',
+    gray_400: '#8D8686',
+    gray_500: '#D7D5D5',
+    gray_600: '#E6E5E5',
+    gray_700: '#EDEDED',
+    gray_800: '#F3F2F2',
+    gray_900: '#FAFAFA',
+
+    yellow: '#DBAC2C',
+    dark_yellow: '#C47F17',
+    purple: '#8047F8',
+    dark_purple: '#4B2995',
+    light_purple: '#EBE5F9',
+
+    red: '#E8BAAB',
+    dark_red: '#C44117',
+    light_red: '#F2DFD8',
+  },
+  fonts: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold',
+    title: 'Baloo2_700Bold',
+  },
+}
