8baf0eeef62a5d4f370d8fa53ce140e7aa3799f6
Luiz Pires <lpiresjunior@gmail.com>
2023-06-19 23:48:26 -0300

    refactor: move styles to a dedicated stylesheet

--- src/components/featuredCoffeeCard.tsx
+++ src/components/featuredCoffeeCard.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 import { FeaturedCoffee } from '../data/featured-coffee'
 import { theme } from '../styles/theme'
@@ -12,97 +12,89 @@ export function FeaturedCoffeeCard({ index, data }: ListProps) {
   const CoffeeImg = data.svg
 
   return (
-    <View
-      style={{
-        width: 208,
-        height: 262,
-        backgroundColor: theme.colors.gray_800,
-        alignItems: 'center',
-        marginTop: 30,
-        paddingHorizontal: 16,
-        paddingBottom: 20,
-        borderRadius: 6,
-        borderTopRightRadius: 36,
-        borderBottomLeftRadius: 36,
-        borderWidth: 1,
-        borderColor: theme.colors.gray_700,
-        shadowColor: 'black',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.05,
-        shadowRadius: 8,
-      }}
-    >
-      <CoffeeImg style={{ marginTop: -30 }} />
+    <View style={styles.container}>
+      <CoffeeImg style={styles.image} />
 
-      <View
-        style={{
-          paddingVertical: 4,
-          paddingHorizontal: 8,
-          backgroundColor: theme.colors.light_purple,
-          borderRadius: 100,
-          marginTop: 8,
-        }}
-      >
-        <Text
-          style={{
-            fontFamily: theme.fonts.bold,
-            fontSize: 10,
-            color: theme.colors.dark_purple,
-            textTransform: 'uppercase',
-          }}
-        >
-          {data.type}
-        </Text>
+      <View style={styles.coffeeTypeTextContainer}>
+        <Text style={styles.coffeeTypeText}>{data.type}</Text>
       </View>
 
-      <Text
-        style={{
-          fontFamily: theme.fonts.title,
-          fontSize: 20,
-          color: theme.colors.gray_200,
-          marginTop: 14,
-        }}
-      >
-        {data.name}
-      </Text>
+      <Text style={styles.coffeeName}>{data.name}</Text>
 
-      <Text
-        style={{
-          fontFamily: theme.fonts.regular,
-          fontSize: 12,
-          color: theme.colors.gray_400,
-          textAlign: 'center',
-          marginTop: 4,
-        }}
-      >
-        {data.description}
-      </Text>
+      <Text style={styles.description}>{data.description}</Text>
 
-      <View
-        style={{ flexDirection: 'row', alignItems: 'center', marginTop: 14 }}
-      >
-        <Text
-          style={{
-            fontFamily: theme.fonts.regular,
-            fontSize: 14,
-            color: theme.colors.dark_yellow,
-          }}
-        >
-          R${' '}
-        </Text>
-        <Text
-          style={{
-            fontFamily: theme.fonts.title,
-            fontSize: 24,
-            color: theme.colors.dark_yellow,
-          }}
-        >
-          {data.price}
-        </Text>
+      <View style={styles.priceContainer}>
+        <Text style={styles.pricePrefix}>R$ </Text>
+        <Text style={styles.price}>{data.price}</Text>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 208,
+    height: 262,
+    backgroundColor: theme.colors.gray_800,
+    alignItems: 'center',
+    marginTop: 30,
+    paddingHorizontal: 16,
+    paddingBottom: 20,
+    borderRadius: 6,
+    borderTopRightRadius: 36,
+    borderBottomLeftRadius: 36,
+    borderWidth: 1,
+    borderColor: theme.colors.gray_700,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 8,
+  },
+  image: {
+    marginTop: -30,
+  },
+  coffeeTypeTextContainer: {
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    backgroundColor: theme.colors.light_purple,
+    borderRadius: 100,
+    marginTop: 8,
+  },
+  coffeeTypeText: {
+    fontFamily: theme.fonts.bold,
+    fontSize: 10,
+    color: theme.colors.dark_purple,
+    textTransform: 'uppercase',
+  },
+  coffeeName: {
+    fontFamily: theme.fonts.title,
+    fontSize: 20,
+    color: theme.colors.gray_200,
+    marginTop: 14,
+  },
+  description: {
+    fontFamily: theme.fonts.regular,
+    fontSize: 12,
+    color: theme.colors.gray_400,
+    textAlign: 'center',
+    marginTop: 4,
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 14,
+  },
+  pricePrefix: {
+    fontFamily: theme.fonts.regular,
+    fontSize: 14,
+    color: theme.colors.dark_yellow,
+  },
+  price: {
+    fontFamily: theme.fonts.title,
+    fontSize: 24,
+    color: theme.colors.dark_yellow,
+  },
+})
