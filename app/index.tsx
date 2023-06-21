f5c3f2fcaffa06fe40134c50ab3aaf9cdbcdceb6
Luiz Pires <lpiresjunior@gmail.com>
2023-06-21 02:58:32 -0300

    feat: create section list card

--- app/index.tsx
+++ app/index.tsx
@@ -108,22 +108,99 @@ export default function Index() {
         }
         sections={coffeeList}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Animated.View
-            entering={SlideInDown.delay(1000).duration(1000)}
-            style={{ marginVertical: 40, paddingHorizontal: 32 }}
-          >
-            <Text>{item.name}</Text>
-          </Animated.View>
-        )}
         renderSectionHeader={({ section: { title } }) => (
           <Animated.Text
             entering={SlideInDown.delay(1000).duration(1000)}
-            style={{ color: 'red', paddingHorizontal: 32 }}
+            style={{
+              fontFamily: theme.fonts.title,
+              fontSize: 14,
+              color: theme.colors.gray_400,
+              paddingHorizontal: 32,
+              marginBottom: 24,
+            }}
           >
             {title}
           </Animated.Text>
         )}
+        renderItem={({ item }) => {
+          const CoffeeImg = item.svg
+
+          return (
+            <Animated.View
+              entering={SlideInDown.delay(1000).duration(1000)}
+              style={{
+                flexDirection: 'row',
+                marginHorizontal: 32,
+                marginBottom: 32,
+                padding: 16,
+                gap: 12,
+
+                backgroundColor: theme.colors.gray_800,
+                borderRadius: 6,
+                borderTopRightRadius: 36,
+                borderBottomLeftRadius: 36,
+                borderWidth: 1,
+                borderColor: theme.colors.gray_700,
+                shadowColor: 'black',
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.05,
+                shadowRadius: 8,
+              }}
+            >
+              <CoffeeImg style={{ marginTop: -32, marginLeft: -8 }} />
+              <View style={{ flex: 1, width: '100%' }}>
+                <Text
+                  style={{
+                    fontFamily: theme.fonts.title,
+                    fontSize: 16,
+                    color: theme.colors.gray_200,
+                  }}
+                >
+                  {item.name}
+                </Text>
+                <Text
+                  style={{
+                    fontFamily: theme.fonts.regular,
+                    fontSize: 12,
+                    color: theme.colors.gray_400,
+                    marginTop: 4,
+                  }}
+                >
+                  {item.description}
+                </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginTop: 8,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: theme.fonts.regular,
+                      fontSize: 14,
+                      color: theme.colors.dark_yellow,
+                    }}
+                  >
+                    R${' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontFamily: theme.fonts.title,
+                      fontSize: 20,
+                      color: theme.colors.dark_yellow,
+                    }}
+                  >
+                    {item.price}
+                  </Text>
+                </View>
+              </View>
+            </Animated.View>
+          )
+        }}
       />
     </View>
   )
