c1e5af9035c79d5de8d75218659468cd54792473
Luiz Pires <lpiresjunior@gmail.com>
2023-06-24 00:06:30 -0300

    refactor: coffee card styles

--- src/components/coffeeCard.tsx
+++ src/components/coffeeCard.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import Animated, { SlideInDown } from 'react-native-reanimated'
 
 import { Coffee } from '../data/coffee-list'
@@ -14,76 +14,67 @@ export function CoffeeCard({ item }: CardProps) {
   return (
     <Animated.View
       entering={SlideInDown.delay(1000).duration(1000)}
-      style={{
-        flexDirection: 'row',
-        marginHorizontal: 32,
-        marginBottom: 32,
-        padding: 16,
-        gap: 12,
-
-        backgroundColor: theme.colors.gray_800,
-        borderRadius: 6,
-        borderTopRightRadius: 36,
-        borderBottomLeftRadius: 36,
-        borderWidth: 1,
-        borderColor: theme.colors.gray_700,
-        shadowColor: 'black',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.05,
-        shadowRadius: 8,
-      }}
+      style={styles.container}
     >
       <CoffeeImg style={{ marginTop: -32, marginLeft: -8 }} />
       <View style={{ flex: 1, width: '100%' }}>
-        <Text
-          style={{
-            fontFamily: theme.fonts.title,
-            fontSize: 16,
-            color: theme.colors.gray_200,
-          }}
-        >
-          {item.name}
-        </Text>
-        <Text
-          style={{
-            fontFamily: theme.fonts.regular,
-            fontSize: 12,
-            color: theme.colors.gray_400,
-            marginTop: 4,
-          }}
-        >
-          {item.description}
-        </Text>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginTop: 8,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: theme.fonts.regular,
-              fontSize: 14,
-              color: theme.colors.dark_yellow,
-            }}
-          >
-            R${' '}
-          </Text>
-          <Text
-            style={{
-              fontFamily: theme.fonts.title,
-              fontSize: 20,
-              color: theme.colors.dark_yellow,
-            }}
-          >
-            {item.price}
-          </Text>
+        <Text style={styles.cardTitle}>{item.name}</Text>
+        <Text style={styles.cardDescription}>{item.description}</Text>
+        <View style={styles.priceContainer}>
+          <Text style={styles.price$}>R$ </Text>
+          <Text style={styles.price}>{item.price}</Text>
         </View>
       </View>
     </Animated.View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginHorizontal: 32,
+    marginBottom: 32,
+    padding: 16,
+    gap: 12,
+
+    backgroundColor: theme.colors.gray_800,
+    borderRadius: 6,
+    borderTopRightRadius: 36,
+    borderBottomLeftRadius: 36,
+    borderWidth: 1,
+    borderColor: theme.colors.gray_700,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 8,
+  },
+  cardTitle: {
+    fontFamily: theme.fonts.title,
+    fontSize: 16,
+    color: theme.colors.gray_200,
+  },
+  cardDescription: {
+    fontFamily: theme.fonts.regular,
+    fontSize: 12,
+    color: theme.colors.gray_400,
+    marginTop: 4,
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  price$: {
+    fontFamily: theme.fonts.regular,
+    fontSize: 14,
+    color: theme.colors.dark_yellow,
+  },
+  price: {
+    fontFamily: theme.fonts.title,
+    fontSize: 20,
+    color: theme.colors.dark_yellow,
+  },
+})
