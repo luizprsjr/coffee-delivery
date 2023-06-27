0b1544d869238390d316acf0cf168849f58f04c0
Luiz Pires <lpiresjunior@gmail.com>
2023-06-27 03:58:23 -0300

    feat: create redux cart slice

---
+++ src/store/cart.ts
@@ -0,0 +1,50 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface Item {
+  id: string
+  size: number
+  quantity: number
+}
+
+interface CartState {
+  items: Item[]
+}
+
+const cartSlice = createSlice({
+  name: 'shoppingCart',
+  initialState: {
+    items: [],
+  } as CartState,
+  reducers: {
+    addItem: (state, action: PayloadAction<Item>) => {
+      const item = state.items.find((item) => action.payload.id === item.id)
+
+      if (item) {
+        item.quantity += action.payload.quantity
+      } else {
+        state.items.push(action.payload)
+      }
+    },
+    removeItem: (state, action: PayloadAction<string>) => {
+      state.items = state.items.filter((item) => item.id !== action.payload)
+    },
+    increaseQuantity: (state, action: PayloadAction<string>) => {
+      const itemId = action.payload
+      const item = state.items.find((item) => item.id === itemId)
+      if (item) {
+        item.quantity += 1
+      }
+    },
+    decreaseQuantity: (state, action: PayloadAction<string>) => {
+      const itemId = action.payload
+      const item = state.items.find((item) => item.id === itemId)
+      if (item && item.quantity > 0) {
+        item.quantity -= 1
+      }
+    },
+  },
+})
+
+export const { addItem, removeItem, increaseQuantity, decreaseQuantity } =
+  cartSlice.actions
+export default cartSlice.reducer
